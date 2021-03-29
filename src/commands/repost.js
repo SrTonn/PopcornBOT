02d2adcaf7043229ee9d0ec4193b97d787358673
Wellington Rodrigues <welliington.rodriigues@gmail.com>
2021-03-29 01:32:17 -0300

    fix: fix repost file
    fix error in repost file when the BOT hears any channel posts or messages than not be reference to
    repost command

--- src/commands/repost.js
+++ src/commands/repost.js
@@ -10,58 +10,70 @@ module.exports = bot => {
         [
           ctx.update.channel_post,
           ctx.update.channel_post.text === command[0],
-          ctx.update.channel_post.reply_to_message.video != null ||
-          ctx.update.channel_post.reply_to_message.photo != null
-        ] : [false],
-      privateConditionArray = ctx.chat.type !== 'channel' ?
+          !!ctx.update.channel_post.reply_to_message.video ||
+          !!ctx.update.channel_post.reply_to_message.photo
+        ] : [false]
+
+      var privateConditionArray = ctx.chat.type !== 'channel' ?
         [
           ctx.chat.type !== 'channel',
           ctx.update.message.text === command[0] ||
           ctx.update.message.text === command[1],
-          ctx.update.message.reply_to_message.video != null ||
-          ctx.update.message.reply_to_message.photo != null
+          !!ctx.update.message.reply_to_message.video ||
+          !!ctx.update.message.reply_to_message.photo
         ] : [false]
 
-        // Verifica se o texto contém o commando definido acima &&
-        // Verifica se o método responder foi utilizado &&
-        // Verifica se é post em canal
-        if (!channelConditionArray.includes((false))) {
-
-          let msgId = ctx.update.channel_post.message_id,
-              caption = ctx.update.channel_post.reply_to_message.caption,
-              file_id = ctx.update.channel_post.reply_to_message.video ?
-                        ctx.update.channel_post.reply_to_message.video.file_id :
-                        ctx.update.channel_post.reply_to_message.photo[1].file_id
-
-          ctx.update.channel_post.reply_to_message.video ?
-            ctx.replyWithVideo(file_id, {caption: caption}) :
-            ctx.replyWithPhoto(file_id, {caption: caption})
-          ctx.telegram.deleteMessage(ctx.chat.id, msgId)
-
+      // Verifica se o texto contém o commando definido acima &&
+      // Verifica se o método responder foi utilizado &&
+      // Verifica se é post em canal
+      if (!channelConditionArray.includes((false))) {
+  
+        let msgId = ctx.update.channel_post.message_id,
+          caption = ctx.update.channel_post.reply_to_message.caption,
+          file_id = ctx.update.channel_post.reply_to_message.video ?
+            ctx.update.channel_post.reply_to_message.video.file_id :
+            ctx.update.channel_post.reply_to_message.photo[1].file_id
+  
+        console.log(file_id)
+        console.log(file_id.length)
+        caption = file_id.length === 75 ? 
+          caption.replace(/\s+-?\s+(@PopcornBR)/i, ' $1') :
+          caption
+  
+        ctx.update.channel_post.reply_to_message.video ?
+          ctx.replyWithVideo(file_id, {caption: caption}) :
+          ctx.replyWithPhoto(file_id, {caption: caption})
+        ctx.telegram.deleteMessage(ctx.chat.id, msgId)
+  
         // Verifica se o texto contém o commando definido acima &&
         // Verifica se o método responder foi utilizado &&
         // Verifica se é post em chat privado
         // Verifica se a msg é do tipo 'photo' ou 'video'
-        } else if (!privateConditionArray.includes((false))) {
-
-          let msgId = ctx.update.message.message_id,
-              caption = ctx.message.reply_to_message.caption,
-              file_id = ctx.update.message.reply_to_message.video ?
-                        ctx.update.message.reply_to_message.video.file_id :
-                        ctx.update.message.reply_to_message.photo[1].file_id
-
-          // Verifica o tipo e envia o comando conforme o resultado da condição
-          ctx.update.message.reply_to_message.video ?
-            ctx.replyWithVideo(file_id, {caption: caption}) :
-            ctx.replyWithPhoto(file_id, {caption: caption})
-          ctx.telegram.deleteMessage(ctx.chat.id, msgId)
-
-        }
+      } else if (!privateConditionArray.includes((false))) {
+  
+        let msgId = ctx.update.message.message_id,
+          caption = ctx.message.reply_to_message.caption,
+          file_id = ctx.update.message.reply_to_message.video ?
+            ctx.update.message.reply_to_message.video.file_id :
+            ctx.update.message.reply_to_message.photo[1].file_id
+  
+        console.log('private IF')
+        console.log(file_id)
+        console.log(file_id.length)
+        caption = file_id.length >= 70 && file_id.length <= 80 ? 
+          caption.replace(/\s+-?\s+(@PopcornBR)/i, ' $1') :
+          caption
+        
+        // Verifica o tipo e envia o comando conforme o resultado da condição
+        ctx.update.message.reply_to_message.video ?
+          ctx.replyWithVideo(file_id, {caption: caption}) :
+          ctx.replyWithPhoto(file_id, {caption: caption})
+        ctx.telegram.deleteMessage(ctx.chat.id, msgId)
+      }
     } catch (error) {
-
+      
       // console.log(error)
     }
-
     next()
   })
 }

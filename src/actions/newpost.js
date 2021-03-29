ed80a732e8167ff6bb07979ab71c964eed01c2cf
Wellington Rodrigues <welliington.rodriigues@gmail.com>
2021-03-29 01:52:15 -0300

    feat: visual warnings about order status and new regex filters added to the code

--- src/actions/newpost.js
+++ src/actions/newpost.js
@@ -11,6 +11,7 @@ const { sleep, compareValues, getRandomInt } = require('../../libs/functions')
 const stage = new Stage()
 
 module.exports = bot => {
+  // let hasUserName = false
   let res = ''
 
   bot.use(stage.middleware())
@@ -25,6 +26,7 @@ module.exports = bot => {
       console.log(error)
     }
     console.log('Iniciando newpost em', chatInfo.title)
+    // hasUserName = chatInfo.username || true
 
     ctx.scene.reset()
     ctx.scene.session = {}
@@ -53,15 +55,18 @@ module.exports = bot => {
       let msgId = ctx.update.message.message_id,
         videoCap = ctx.update.message.caption,
         videoId = ctx.update.message.video.file_id,
-        chatId = ctx.chat.id,
+        chatId = ctx.chat.id
+      videoCap = videoCap
+        .replace(/T(\d{2}E\d{2})/g, 'S$1')
+        .replace(/\s+-?\s+(@PopcornBR)/i, ' $1')
 
-        // Modelo padrão para armazenar os dados em um objeto
-        padrao = {
-          msgId: msgId,
-          videoCap: videoCap,
-          videoId: videoId,
-          chatId: chatId
-        }
+      // Modelo padrão para armazenar os dados em um objeto
+      let padrao = {
+        msgId: msgId,
+        videoCap: videoCap,
+        videoId: videoId,
+        chatId: chatId
+      }
       posts.push(padrao)
 
       if (posts.filter(x => x.chatId === ctx.chat.id).length === 21) {
@@ -105,11 +110,11 @@ module.exports = bot => {
 
         ctx.reply('ERROR nenhum vídeo encontrado!\nEnvie algum arquivo de vídeo para começar.',
           Extra
-          .markdown()
-          .markup(
-            Markup.keyboard(['Done', 'Cancelar'])
-            .resize()
-          )
+            .markdown()
+            .markup(
+              Markup.keyboard(['Done', 'Cancelar'])
+                .resize()
+            )
         )
         ctx.scene.enter('que 1')
 
@@ -126,6 +131,10 @@ module.exports = bot => {
       )
 
       leave()
+    })
+    question1.hears(['Forçar','forced','fsend','fs'], ctx => {
+      posts = ''
+      ctx.scene.enter('que 3')
     })
 
     // question2 scene
@@ -147,29 +156,32 @@ module.exports = bot => {
       }
       for (let i in res)
 
-      if (ctx.chat.id === res[i].chatId) {
-        resumo += '\n 📹 ' + res[i].videoCap
-      }
+        if (ctx.chat.id === res[i].chatId) {
+          resumo += '\n 📹 ' + res[i].videoCap
+        }
 
       let countPost = res.filter(x => x.chatId === ctx.chat.id).length + cover.length
-
       if (countPost <= 20) {
         ctx.reply(`Temos ${countPost} midia(s), escolha o que fazer.\n${resumo}`,
-        Extra
-          .markdown()
-          .markup(
-            Markup.keyboard(['Enviar', 'Adicionar Mais', 'Cancelar'])
-            .resize()
-          )
+          Extra
+            .markdown()
+            .markup(
+              Markup.keyboard(['Enviar', 'Adicionar Mais', 'Cancelar'])
+                .resize()
+            )
         )
       } else {
         ctx.reply(`Temos ${countPost} midia(s), escolha o que fazer.\n${resumo}`,
           Extra
-          .markdown()
-          .markup(
-            Markup.keyboard(['Enviar - modo seguro', 'Forçar Envio Rápido', 'Adicionar Mais', 'Cancelar'])
-            .resize()
-          )
+            .markdown()
+            .markup(
+              Markup.keyboard([
+                'Enviar - modo seguro',
+                'Forçar Envio Rápido',
+                'Adicionar Mais',
+                'Cancelar'
+              ]).resize()
+            )
         )
       }
     })
@@ -181,43 +193,43 @@ module.exports = bot => {
       let countPost = posts.filter(x => x.chatId === ctx.chat.id).length
 
       for (let i in res)
-      if (ctx.chat.id === res[i].chatId) {
+        if (ctx.chat.id === res[i].chatId) {
 
-        if (i === parseInt(res.length / 2) && res.length >= 20) {
-          ctx.reply(`*Parte 1 com ${parseInt(res.length / 2)} posts* exibido(s),` +
+          if (i === parseInt(res.length / 2) && res.length >= 20) {
+            ctx.reply(`*Parte 1 com ${parseInt(res.length / 2)} posts* exibido(s),` +
           'escolha entre uma das opções para prosseguir.',
             Extra.markdown().markup(
               Markup.keyboard(['Enviar', 'Cancelar', 'Adicionar Mais'])
-              .resize()
+                .resize()
             )
-          )
-          break
+            )
+            break
+          }
+
+          await ctx.telegram.sendVideo(res[i].chatId, res[i].videoId, {caption: res[i].videoCap})
+
+          sleep(getRandomInt(600, 1500))
+
+          if (i === parseInt(res.length / 2) && res.length >= 20) {
+            bot.telegram.sendChatAction(ctx.chat.id, 'typing')
+            sleep(getRandomInt(3000, 9000))
+          }
         }
-
-        await ctx.telegram.sendVideo(res[i].chatId, res[i].videoId, {caption: res[i].videoCap})
-
-        sleep(getRandomInt(600, 1500))
-
-        if (i === parseInt(res.length / 2) && res.length >= 20) {
-          bot.telegram.sendChatAction(ctx.chat.id, 'typing')
-          sleep(getRandomInt(3000, 9000))
-        }
-      }
       sleep(getRandomInt(1000, 3000))
 
       if (res.length < 20) {
         ctx.reply(`${countPost} posts exibido(s), escolha entre uma das opções para prosseguir.`,
           Extra.markdown().markup(
-          Markup.keyboard(['Enviar', 'Cancelar', 'Adicionar Mais']).resize()
-        ))
+            Markup.keyboard(['Enviar', 'Cancelar', 'Adicionar Mais']).resize()
+          ))
       } else {
         ctx.reply(`${countPost} posts exibido(s), escolha entre uma das opções para prosseguir.`,
           Extra
-          .markdown()
-          .markup(
-            Markup.keyboard(['Enviar 1ª parte', 'Cancelar', 'Adicionar Mais'])
-            .resize()
-          )
+            .markdown()
+            .markup(
+              Markup.keyboard(['Enviar 1ª parte', 'Cancelar', 'Adicionar Mais'])
+                .resize()
+            )
         )
       }
     })
@@ -225,11 +237,11 @@ module.exports = bot => {
     question2.hears('Adicionar Mais', ctx => {
       ctx.reply('Certo, agora pode enviar mais mídias!\n Ao terminar clique em *done*.',
         Extra
-        .markdown()
-        .markup(
-          Markup.keyboard(['Done'])
-          .resize()
-        )
+          .markdown()
+          .markup(
+            Markup.keyboard(['Done'])
+              .resize()
+          )
       )
       ctx.scene.enter('que 1')
     })
@@ -252,85 +264,66 @@ module.exports = bot => {
     // question3 scene
     question3.enter(async (ctx) => {
 
-        // Pega o ID da msg do usuário e acrescenta +1 para ter o ID da próxima msg do BOT
-        let messageId = ctx.update.message.message_id + 1
-        ctx.reply('🕐 Enviando...', {
-          reply_markup: {remove_keyboard: true}
-        })
-        bot.telegram.sendChatAction(ctx.chat.id, 'typing')
-
-        // Postar resultado no canal!
-        // Se tiver imagem, envia primeiro
-        if (cover.findIndex(x => x.chatId === ctx.chat.id) !== -1) {
-          let i = cover.findIndex(x => x.chatId === ctx.chat.id)
-          await ctx.telegram.sendPhoto(channelId, cover[i].photoId, {caption: cover[i].photoCap})
-        }
-
-        // Envia todos os vídeos
-        for (let i in res)
-        if (ctx.chat.id === res[i].chatId) {
-
-          if ((i >= 19 && cover.findIndex(x => x.chatId === ctx.chat.id) !== -1) ||
-          (i >= 20 && cover.findIndex(x => x.chatId === ctx.chat.id) === -1)) break
-          await ctx.telegram.sendVideo(channelId, res[i].videoId, {caption: res[i].videoCap})
-          sleep(getRandomInt(100, 600))
-
-        }
-
-        // Apagar mensagem 'enviando...'
-        await ctx.telegram.deleteMessage(ctx.chat.id, messageId)
-        await ctx.reply('Feito!',
-          {
-            reply_markup: Markup.inlineKeyboard([
-              Markup.urlButton(
-                'Visualizar Post no Canal.',
-                't.me/' + chatInfo.username
-              )
-            ])
-          }
-        )
-
-        ctx.reply('Criar novo post? 👇',
-        Markup.inlineKeyboard([
-          Markup.callbackButton('Criar Post', 'newpost')
-        ]).extra()
-        )
-        ctx.scene.leave()
-    })
-
-    question4.enter(async (ctx) => {
-
-      // Pega o ID da msg do usuário e acrescenta +1 para ter o ID da próxima msg do BOT
-      let messageId = ctx.update.message.message_id + 1
-      ctx.reply('🕐 Enviando...', {
+      let messageId = ''
+      await ctx.reply('Preparando...', {
         reply_markup: {remove_keyboard: true}
+      }).then(({message_id}) => {
+        messageId = message_id
       })
+
       bot.telegram.sendChatAction(ctx.chat.id, 'typing')
 
       // Postar resultado no canal!
       // Se tiver imagem, envia primeiro
+      console.log('lin278')
       if (cover.findIndex(x => x.chatId === ctx.chat.id) !== -1) {
         let i = cover.findIndex(x => x.chatId === ctx.chat.id)
-        await ctx.telegram.sendPhoto(channelId, cover[i].photoId, {caption: cover[i].photoCap})
+        await ctx.telegram.sendPhoto(channelId, cover[i].photoId, 
+          {
+            caption: cover[i].photoCap,
+          }
+        )
       }
-
-      // Envia todos os vídeos
-      for (let i in res)
-      if (ctx.chat.id === res[i].chatId) {
-        await ctx.telegram.sendVideo(channelId, res[i].videoId, {caption: res[i].videoCap})
-        sleep(getRandomInt(1600, 2000))
-        if (i >= 15 && i !== res.length) {
-          sleep(3050)
-        }
-      }
-
-      // Apagar mensagem 'enviando...'
+      
+      await ctx.reply('Enviando...')
       await ctx.telegram.deleteMessage(ctx.chat.id, messageId)
-      await ctx.reply('Feito!',
+      messageId++
+      console.log('res.length',res.length)
+      // Envia todos os vídeos no modo rápido
+      for (let i in res)
+        if (ctx.chat.id === res[i].chatId) {
+          let x = i * 100 / res.length
+          console.log('x=>',x,'i=>',i)
+          if ((i >= 19 && cover.findIndex(x => x.chatId === ctx.chat.id) !== -1) ||
+          (i >= 20 && cover.findIndex(x => x.chatId === ctx.chat.id) === -1)) break
+
+          await ctx.telegram.sendVideo(channelId, res[i].videoId,
+            {
+              caption: res[i].videoCap
+            }
+          )
+
+          if (i % 2 == 0) {
+            await bot.telegram.editMessageText(
+              ctx.chat.id, 
+              messageId, 
+              messageId, 
+              `${parseInt(x)}% concluído.`
+            )
+          }
+          sleep(getRandomInt(100, 600))
+        }
+
+      // Editar msg avisando que o post foi concluído.
+      await bot.telegram.editMessageText(
+        ctx.chat.id, 
+        messageId, 
+        messageId, 
+        '100% concluído.',
         {reply_markup: Markup.inlineKeyboard([
           Markup.urlButton(
             'Visualizar Post no Canal.',
-            't.me/' + chatInfo.username
+            't.me/c/' + String(channelId).slice(4) + '/99999'
           )
         ]
         )}
@@ -340,7 +333,78 @@ module.exports = bot => {
         Markup.inlineKeyboard([
           Markup.callbackButton('Criar Post', 'newpost')
         ]).extra()
+      ).then(({message_id}) => {
+        ctx.telegram.pinChatMessage(ctx.chat.id, message_id)
+      })
+      ctx.scene.leave()
+    })
+
+    question4.enter(async (ctx) => {
+
+      // Pega o ID da msg do usuário e acrescenta +1 para ter o ID da próxima msg do BOT
+      let messageId = ''
+      await ctx.reply('Preparando post.', {
+        reply_markup: {remove_keyboard: true}
+      }).then(({message_id}) => {
+        messageId = message_id
+      })
+      bot.telegram.sendChatAction(ctx.chat.id, 'typing')
+
+
+      await ctx.reply('Enviando...')
+      await ctx.telegram.deleteMessage(ctx.chat.id, messageId)
+      messageId++
+
+      // Postar resultado no canal!
+      // Se tiver imagem, envia primeiro
+      if (cover.findIndex(x => x.chatId === ctx.chat.id) !== -1) {
+        let i = cover.findIndex(x => x.chatId === ctx.chat.id)
+        await ctx.telegram.sendPhoto(channelId, cover[i].photoId, {caption: cover[i].photoCap})
+      }
+
+      // Envia todos os vídeos no modo seguro
+      for (let i in res)
+        if (ctx.chat.id === res[i].chatId) {
+          await ctx.telegram.sendVideo(channelId, res[i].videoId, {caption: res[i].videoCap})
+          sleep(getRandomInt(1600, 2000))
+          if (i >= 15 && i !== res.length) {
+            sleep(3050)
+          }
+
+          let x = i * 100 / res.length
+
+          if (i % 2 == 0) {
+            await bot.telegram.editMessageText(
+              ctx.chat.id, 
+              messageId, 
+              messageId, 
+              `${parseInt(x)}% concluído.`
+            )
+          }
+        }
+
+      // Editar msg avisando que o post foi concluído.
+      await bot.telegram.editMessageText(
+        ctx.chat.id, 
+        messageId, 
+        messageId, 
+        '100% concluído.',
+        {reply_markup: Markup.inlineKeyboard([
+          Markup.urlButton(
+            'Visualizar Post no Canal.',
+            't.me/c/' + String(channelId).slice(4) + '/99999'
+          )
+        ]
+        )}
       )
+
+      ctx.reply('Criar novo post? 👇',
+        Markup.inlineKeyboard([
+          Markup.callbackButton('Criar Post', 'newpost')
+        ]).extra()
+      ).then(({message_id}) => {
+        ctx.telegram.pinChatMessage(ctx.chat.id, message_id)
+      })
       ctx.scene.leave()
     })
 
@@ -354,7 +418,7 @@ module.exports = bot => {
         ]).extra()
       )
 
-      leave()
+      ctx.scene.leave()
     })
 
     // Scene 0 - Verificar se o usuário já tem dados na memória e limpar
@@ -379,26 +443,26 @@ module.exports = bot => {
       ctx.update.callback_query.id,
       'Selecione o *Canal*.',
       Extra.markdown()
-      .markup((m) =>
-        m.inlineKeyboard([
-        m.callbackButton(chatInfo.title, chatInfo.id)])
-      )
+        .markup((m) =>
+          m.inlineKeyboard([
+            m.callbackButton(chatInfo.title, chatInfo.id)])
+        )
     )
 
     // parte destinado a criar a capa do post futuramente
     bot.action(channelId, async (ctx) => {
       await ctx.deleteMessage()
       await ctx.reply('Agora envie um ou mais vídeos, quando concluir clique em *done*',
-      Extra
-        .markdown()
-        .markup(
-          Markup.keyboard([
-            'Done',
-            'Cancelar'
-          ])
-          .resize()
-          .oneTime()
-        )
+        Extra
+          .markdown()
+          .markup(
+            Markup.keyboard([
+              'Done',
+              'Cancelar'
+            ])
+              .resize()
+              .oneTime()
+          )
       )
       ctx.scene.enter('que 0')
     })
